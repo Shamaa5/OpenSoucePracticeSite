2b5be559ccfbdbe98a45e42df9d4a1dde88aea5a
Shamil <djeims995@gmail.com>
2021-11-10 19:54:28 +0300

    start

---
+++ src/redux/index.js
@@ -0,0 +1,15 @@
+import {createLogger} from "redux-logger";
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import thunk from "redux-thunk";
+
+
+const logger = createLogger({
+    diff: true,
+    collapsed: true,
+});
+
+const rootReducer = combineReducers({})
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+
+export default store
