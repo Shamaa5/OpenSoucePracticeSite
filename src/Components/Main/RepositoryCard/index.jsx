fde7ce68633d2db971ab35807b61e77cd638efd2
Shamil <djeims995@gmail.com>
2021-11-28 05:03:05 +0300

    Added route to path Card

--- src/Components/Main/RepositoryCard/index.jsx
+++ src/Components/Main/RepositoryCard/index.jsx
@@ -5,11 +5,11 @@ import { Skeleton } from 'antd';
 import { loadRepository } from '../../../redux/actions';
 import { StarFilled } from '@ant-design/icons';
 
-function RepositoryCard(props) {
+function RepositoryCard() {
+  const repository = useSelector((state) => state.repositories.repository);
   const params = useParams();
   const loading = useSelector((state) => state.repositories.loadingRerository);
   const dispatch = useDispatch();
-  const repository = useSelector((state) => state.repositories.repository);
   useEffect(() => {
     if (params.id !== undefined) {
       dispatch(loadRepository(params.id));
@@ -22,17 +22,17 @@ function RepositoryCard(props) {
   return (
     <div>
       <h2> {repository['full_name']}</h2>
-      <div className="repoCardOwnerAvatar"></div>
+      <div />
       <div>{repository.description}</div>
       <div>
         <StarFilled style={{ color: '#f1b667' }} />:{' '}
-        {repository.stargazers_count}
+        {repository['stargazers_count']}
       </div>
       <div>
         <a href={repository['html_url']}>{repository['html_url']}</a>
       </div>
-      <div>Created : {repository.created_at}</div>
-      <div>Лицензия</div>
+      <div>Created : {repository['created_at']}</div>
+      {/*<div>{repository.license['name']}</div>*/}
       <div>Open Issues</div>
       <div>Most Recent Commit</div>
       <div>Site</div>
